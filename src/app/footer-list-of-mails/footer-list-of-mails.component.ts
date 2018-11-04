d8324a8d0fea7be64a0794e9e86adeeb7a6c8910
Chantarel <martakossowski@gmail.com>
2018-11-04 16:47:02 +0100

    usunięcie załączenia pliku

--- src/app/footer-list-of-mails/footer-list-of-mails.component.ts
+++ src/app/footer-list-of-mails/footer-list-of-mails.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer-list-of-mails',
-  templateUrl: './footer-list-of-mails.component.html',
-  styleUrls: ['./footer-list-of-mails.component.css']
+  templateUrl: './footer-list-of-mails.component.html'
 })
 export class FooterListOfMailsComponent implements OnInit {
 

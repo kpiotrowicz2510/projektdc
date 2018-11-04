2b88eef7214d066e9f5ab0b0979a968c6166e270
Chantarel <martakossowski@gmail.com>
2018-11-04 16:47:42 +0100

    usunięcie załączenia pliku

--- src/app/list-of-mails/list-of-mails.component.ts
+++ src/app/list-of-mails/list-of-mails.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-of-mails',
-  templateUrl: './list-of-mails.component.html',
-  styleUrls: ['./list-of-mails.component.css']
+  templateUrl: './list-of-mails.component.html'
   
 })
 export class ListOfMailsComponent implements OnInit {

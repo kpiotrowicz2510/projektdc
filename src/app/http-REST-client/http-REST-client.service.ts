245d4cc65a760f0529f178ad81ed39281652bd79
Przekop <przekop.michal@gmail.com>
2018-11-06 22:11:48 +0100

    http generic client

--- src/app/http-REST-client/http-REST-client.service.ts
+++ src/app/http-REST-client/http-REST-client.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-@Injectable()
+@Injectable({
+    providedIn: 'root',
+})
 export class HttpRESTClientService {
     url: string = 'http://localhost:8080/activiti-service/api';
    
@@ -10,5 +12,18 @@ export class HttpRESTClientService {
     get_tasks() {
         return this.http.get(this.url + '/process/tasks');
     }
+
+    http_get(inUrl: string) {
+        return this.http.get(this.url + inUrl);
+    }
+
+    http_post(inUrl: string, body: any) {
+        this.http.post(this.url + inUrl, body);
+    }
+
+    http_put(inUrl: string, body: any) {
+        this.http.put(this.url + inUrl, body);
+    }
+
 
 }

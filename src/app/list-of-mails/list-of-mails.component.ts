40fc344cdd529d99dde95834ed8650fd170ac71e
Chantarel <martakossowski@gmail.com>
2018-11-10 21:59:28 +0100

    Data Binding

--- src/app/list-of-mails/list-of-mails.component.ts
+++ src/app/list-of-mails/list-of-mails.component.ts
@@ -6,10 +6,64 @@ import { Component, OnInit } from '@angular/core';
   
 })
 export class ListOfMailsComponent implements OnInit {
+   
+  listOfMails: any[] = [];
 
+  constructor() {
+	
+    this.listOfMails.push({
+        "title": "title1",
+        "from": "jan.kowalski@gmail.com"
+      "active": ""
+      });
 
-  constructor() { }
+      this.listOfMails.push({
+        "title": "title2",
+        "from": "jan.kowalski@gmail.com"
+      "active": ""
+      });
 
+      this.listOfMails.push({
+        "title": "title3",
+        "from": "jan.kowalski@gmail.com"
+      "active": ""
+      });
+
+      this.listOfMails.push({
+        "title": "title4",
+        "from": "jan.kowalski@gmail.com"
+      "active": ""
+      });
+
+      this.listOfMails.push({
+        "title": "title5",
+        "from": "jan.kowalski@gmail.com"
+      "active": ""
+      });
+
+      this.listOfMails.push({
+        "title": "title6",
+        "from": "jan.kowalski@gmail.com"
+      "active": ""
+      });
+
+      this.listOfMails.push({
+        "title": "title7",
+        "from": "jan.kowalski@gmail.com"
+      "active": ""
+      });
+  
+  }
+
+  selectMail = function(mail){  
+	
+    for (let i in this.listOfMails) {
+      this.listOfMails[i].active = "";
+    }
+
+    mail.active = "active"
+  }
+  
   ngOnInit() {}
   
 }

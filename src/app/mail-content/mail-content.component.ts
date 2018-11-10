8da3ed0c86f361ef84926f4f9d8ea06f215c23c3
Chantarel <martakossowski@gmail.com>
2018-11-10 22:01:57 +0100

    Data Binding

--- src/app/mail-content/mail-content.component.ts
+++ src/app/mail-content/mail-content.component.ts
@@ -6,8 +6,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MailContentComponent implements OnInit {
 
-  constructor() { }
+  name: String;
+  surname: String;
+  mail: String;
+  date: String;
+  content: String;
 
+  constructor() { 
+	this.name = "Jan";
+	this.surname = "Kowalski";
+	this.mail = "jan.kowalski@gmail.com";
+	this.date = "12.12.2018";
+	this.content = "Hi, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a sagittis dolor. Suspendisse a hendrerit mi. Vivamus a tincidunt arcu, eu maximus mauris. Curabitur consectetur pulvinar diam, id tempor eros condimentum a. Vivamus aliquet neque ut lectus aliquet blandit eget id felis. Vivamus rhoncus sem elit, quis lacinia nisi mollis ut. Mauris id fringilla erat";
+  }
+  
   ngOnInit() {
   }
 
